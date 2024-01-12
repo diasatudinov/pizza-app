5c09f4e16114d07096fa905edad3ec72f1b1df87
Dias <dias.atudinov@gmail.com>
2024-01-13 02:49:28 +0600

    TabBarView

---
+++ pizza app/Module/TabBarModule/View/TabBarView.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarView.swift
+//  pizza app
+//
+//  Created by Dias Atudinov on 13.01.2024.
+//
+
+import UIKit
+
+class TabBarView: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
