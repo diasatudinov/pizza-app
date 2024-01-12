a8ccd6996240cf66b59875255a06c794e274f8cd
Dias <dias.atudinov@gmail.com>
2024-01-13 02:11:27 +0600

    Initial Commit

---
+++ pizza app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pizza app
+//
+//  Created by Dias Atudinov on 13.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
